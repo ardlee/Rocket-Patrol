340017917e1ee7bc33aa8afaee39bea9a723924c
ardlee <ardlee@ucsc.edu>
2023-04-19 15:37:27 -0700

    spaceships included

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -25,6 +25,11 @@ class Play extends Phaser.Scene {
         // add rocket (P1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
 
+        // add spaceships (x3)
+        this.ship1 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
+        this.ship2 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
+        this.ship3 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
+
         // define reserve keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
@@ -38,5 +43,10 @@ class Play extends Phaser.Scene {
         
         // hook up rocket update to the main loop
         this.p1Rocket.update();
+
+        // update spaceships
+        this.ship1.update();
+        this.ship2.update();
+        this.ship3.update();
     }
 }
